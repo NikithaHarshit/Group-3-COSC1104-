b791dce20622858ca91a0c413613da8193fe0e7b
NikithaHarshit <donthinikitha17@gmail.com>
2024-09-27 19:44:55 -0400

    provisioning_loops.py

--- in-class-1/provisioning_loops.py
+++ in-class-1/provisioning_loops.py
@@ -1,7 +1,8 @@
 # provisioning_loops.py
-# Group Members: Nikitha Donthi, Harshit Sonik
+# Group member name: Harshit Sonik (100941462)
+# Group member name: Nikitha Donthi (100953192)
 # Date: 27-09-2024
-# Description: A simple script to simulate cloud resource provisioning.
+# Description: This script simulates cloud resource provisioning using a loop and manages multiple requests.
 
 # constants:
 Total_CPU_Cores = 20 
